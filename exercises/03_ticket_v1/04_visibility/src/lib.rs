d054f202156b21ff944f594a1922163aee78f8a0
Wesley Jon Cheek <w-cheek@satake-japan.co.jp>
2025-03-05 15:32:23 +0900

    Working through exercises

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
-mod ticket {
-    struct Ticket {
-        title: String,
-        description: String,
-        status: String,
+pub mod ticket {
+    pub struct Ticket {
+        pub title: String,
+        pub description: String,
+        pub status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -31,6 +31,7 @@ mod ticket {
         }
     }
 }
+// use ticket;
 
 // TODO: **Exceptionally**, you'll be modifying both the `ticket` module and the `tests` module
 //  in this exercise.
@@ -43,7 +44,7 @@ mod tests {
     // Be careful though! We don't want this function to compile after you have changed
     // visibility to make the use statement compile!
     // Once you have verified that it indeed doesn't compile, comment it out.
-    fn should_not_be_possible() {
+    pub fn should_not_be_possible() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
 
         // You should be seeing this error when trying to run this exercise:
@@ -58,7 +59,7 @@ mod tests {
         assert_eq!(ticket.description, "A description");
     }
 
-    fn encapsulation_cannot_be_violated() {
+    pub fn encapsulation_cannot_be_violated() {
         // This should be impossible as well, with a similar error as the one encountered above.
         // (It will throw a compilation error only after you have commented the faulty line
         // in the previous test - next compilation stage!)
@@ -68,10 +69,10 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
+        // let ticket = Ticket {
+        //     title: "A title".into(),
+        //     description: "A description".into(),
+        //     status: "To-Do".into(),
+        // };
     }
 }
